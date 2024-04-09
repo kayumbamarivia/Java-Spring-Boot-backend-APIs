23201f1186ea1a87af2d1749b2ffa88f6432ef1f
iradukun <mustaphairadukunda@gmail.com>
2024-04-09 13:44:38 +0200

    my first spring CRUD backend apis commit 5

---
+++ src/main/java/com/jmv/studentManagement/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.jmv.studentManagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")  // Specify the path pattern to which this CORS configuration should apply
+                .allowedOrigins("https://jmv-react-spring-boot-apis-frontend.onrender.com")  // Allow requests from your React frontend domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow these HTTP methods
+                .allowedHeaders("*");  // Allow all headers
+    }
+}
