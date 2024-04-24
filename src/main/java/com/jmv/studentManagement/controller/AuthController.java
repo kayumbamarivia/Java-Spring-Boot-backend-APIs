b842f4e9cfc9d321858f9bc87e17605759d0668e
iradukun <mustaphairadukunda@gmail.com>
2024-04-24 08:58:31 +0200

    token is sent in header

--- src/main/java/com/jmv/studentManagement/controller/AuthController.java
+++ src/main/java/com/jmv/studentManagement/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.jmv.studentManagement.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,76 +20,73 @@ import com.jmv.studentManagement.model.User;
 import com.jmv.studentManagement.service.impl.JwtServiceImpl;
 import com.jmv.studentManagement.service.impl.UserServiceImpl;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api")
 public class AuthController {
 
-	private final UserServiceImpl userServiceImpl;
+    private final UserServiceImpl userServiceImpl;
     private final JwtServiceImpl jwtServiceImpl;
 
-	public AuthController(UserServiceImpl userServiceImpl, JwtServiceImpl jwtServiceImpl) {
-		super();
-		this.userServiceImpl = userServiceImpl;
-		this.jwtServiceImpl = jwtServiceImpl;
-	}
+    public AuthController(UserServiceImpl userServiceImpl, JwtServiceImpl jwtServiceImpl) {
+        super();
+        this.userServiceImpl = userServiceImpl;
+        this.jwtServiceImpl = jwtServiceImpl;
+    }
 
-	// Build Login REST API
-	 @PostMapping(value = {"/login", "/signin"})
-	    public ResponseEntity<User> authenticate(@RequestBody LoginDto request, HttpServletResponse response) {
-	        User user = userServiceImpl.login(request);
-	        String token = jwtServiceImpl.generateToken(user);
-	        
-	        Cookie cookie = new Cookie("token", token);
-	        cookie.setHttpOnly(false);
-	        cookie.setPath("/");
-	        response.addCookie(cookie);
-	        return ResponseEntity.ok(user);
-	    }
+    // Build Login REST API
+    @PostMapping(value = {"/login", "/signin"})
+    public ResponseEntity<User> authenticate(@RequestBody LoginDto request) {
+        User user = userServiceImpl.login(request);
+        String token = jwtServiceImpl.generateToken(user);
 
-	// Build Register REST API
-	@PostMapping(value = {"/register", "/signup"})
-	public ResponseEntity<User> register(@RequestBody RegisterDto request){
-		User response = userServiceImpl.register(request);
-		return ResponseEntity.ok(response);
-	}
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
 
-	// Logout API
-	@GetMapping("/token")
-	public ResponseEntity<String> logout(HttpServletRequest request) {
-		String token = extractTokenFromRequest(request);
-			return ResponseEntity.ok("User token is this : "+token);
-	}
+        return new ResponseEntity<>(user, headers, HttpStatus.OK);
+    }
 
-	private String extractTokenFromRequest(HttpServletRequest request) {
-		String authorizationHeader = request.getHeader("Authorization");
-		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-			return authorizationHeader.substring(7);
-		}
-		return null;
-	}
+    // Build Register REST API
+    @PostMapping(value = {"/register", "/signup"})
+    public ResponseEntity<User> register(@RequestBody RegisterDto request){
+        User response = userServiceImpl.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    // Logout API
+    @GetMapping("/token")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
+        String token = extractTokenFromRequest(request);
+        return ResponseEntity.ok("User token is this : "+token);
+    }
+
+    private String extractTokenFromRequest(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7);
+        }
+        return null;
+    }
 
 
-	@GetMapping("/users")
-	public List<User> getAllUsers(){
-		return userServiceImpl.getAllUsers();
-	}
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userServiceImpl.getAllUsers();
+    }
 
-	@GetMapping("/user/{id}/get")
-	public ResponseEntity<User> getUserById(@PathVariable("id") long id){
-		return new ResponseEntity<User>(userServiceImpl.getUserById(id), HttpStatus.OK);
-	}
+    @GetMapping("/user/{id}/get")
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id){
+        return new ResponseEntity<User>(userServiceImpl.getUserById(id), HttpStatus.OK);
+    }
 
-	@DeleteMapping("/user/{id}/delete")
-	public ResponseEntity<String> deleteUserById(@PathVariable("id") long id){
-		userServiceImpl.deleteUserById(id);
-		return new ResponseEntity<String>("User deleted successfully!!", HttpStatus.OK);
-	}
-	
-	// Update User API
+    @DeleteMapping("/user/{id}/delete")
+    public ResponseEntity<String> deleteUserById(@PathVariable("id") long id){
+        userServiceImpl.deleteUserById(id);
+        return new ResponseEntity<String>("User deleted successfully!!", HttpStatus.OK);
+    }
+
+    // Update User API
     @PutMapping("/user/{id}/edit")
     public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
         User updatedUser = userServiceImpl.updateUserById(user, id);
--- src/main/java/com/jmv/studentManagement/filter/JwtAuthFilter.java
+++ src/main/java/com/jmv/studentManagement/filter/JwtAuthFilter.java
@@ -1,4 +1,5 @@
 package com.jmv.studentManagement.filter;
+
 import java.io.IOException;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,53 +15,45 @@ import com.jmv.studentManagement.service.impl.UserDetailsServiceImpl;
 import io.micrometer.common.lang.NonNull;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
-	
-	private final JwtServiceImpl jwtServiceImpl;
-	private final UserDetailsServiceImpl userDetailsServiceImpl;
-	
-	public JwtAuthFilter(JwtServiceImpl jwtServiceImpl, UserDetailsServiceImpl userDetailsServiceImpl) {
-		super();
-		this.jwtServiceImpl = jwtServiceImpl;
-		this.userDetailsServiceImpl = userDetailsServiceImpl;
-	}
 
+    private final JwtServiceImpl jwtServiceImpl;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
+    public JwtAuthFilter(JwtServiceImpl jwtServiceImpl, UserDetailsServiceImpl userDetailsServiceImpl) {
+        super();
+        this.jwtServiceImpl = jwtServiceImpl;
+        this.userDetailsServiceImpl = userDetailsServiceImpl;
+    }
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-        
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    
-                    String username = jwtServiceImpl.extractUsername(token);
-                    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                        UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(username);
-                        if (jwtServiceImpl.isValid(token, userDetails)) {
-                            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                            
-                            authToken.setDetails(
-                                    new WebAuthenticationDetailsSource().buildDetails(request));
-                            
-                            SecurityContextHolder.getContext().setAuthentication(authToken);
-                        }
-                    }
-                    break; 
-                }
+
+        String authorizationHeader = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            token = authorizationHeader.substring(7);
+            username = jwtServiceImpl.extractUsername(token);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(username);
+            if (jwtServiceImpl.isValid(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-        
+
         filterChain.doFilter(request, response);
     }
 }
-
