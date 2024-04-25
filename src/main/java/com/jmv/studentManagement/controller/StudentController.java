4683cfb964b218850755cdb5939da56f15623aaf
iradukun <mustaphairadukunda@gmail.com>
2024-04-25 10:33:15 +0200

    search functionality modified 4

--- src/main/java/com/jmv/studentManagement/controller/StudentController.java
+++ src/main/java/com/jmv/studentManagement/controller/StudentController.java
@@ -124,7 +124,7 @@ public class StudentController {
 	
 	
 	@GetMapping("/student/search")
-    public ResponseEntity<List<Student>> searchStudentsByUserId(@RequestParam long userId, @RequestParam("searchTerm") String query) {
+    public ResponseEntity<List<Student>> searchStudentsByUserId(@RequestParam("searchTerm") String query, @RequestParam("userId") long userId) {
         List<Student> searchResults = studentService.searchByUserId(query, userId);
         return ResponseEntity.ok(searchResults);
     }
