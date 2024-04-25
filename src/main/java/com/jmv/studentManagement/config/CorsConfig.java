79d481e528a4d7fbd3d3d40c48fe3f174dde1fc6
iradukun <mustaphairadukunda@gmail.com>
2024-04-25 13:58:45 +0200

    search functionality modified 7

--- src/main/java/com/jmv/studentManagement/config/CorsConfig.java
+++ src/main/java/com/jmv/studentManagement/config/CorsConfig.java
@@ -8,13 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("https://apis-react-spring-client-kayumba.vercel.app", "http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .exposedHeaders("Access-Control-Allow-Origin");
     }
 }
+
