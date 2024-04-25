5e2e6b2f3f3dc206142b9edb655ec8147797f8b8
iradukun <mustaphairadukunda@gmail.com>
2024-04-25 08:57:01 +0200

    search functionality modified

--- src/main/java/com/jmv/studentManagement/repository/StudentRepository.java
+++ src/main/java/com/jmv/studentManagement/repository/StudentRepository.java
@@ -8,6 +8,6 @@ import com.jmv.studentManagement.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 	List<Student> findByFirstNameContainingOrLastNameContainingOrEmailContaining(String query, String query2, String query3);
-	List<Student> findByFirstNameContainingOrLastNameContainingOrEmailContainingAndUserId(String query1, String query2, String query3, long userId);
+	List<Student> findByUserIdAndFirstNameContainingOrLastNameContainingOrEmailContaining(long userId, String firstName, String lastName, String email);
 	List<Student> findByUserId(long userId);
 }
